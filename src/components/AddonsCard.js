965bba368a981882a3c06d1bf34392ff423d0db2
eng-milan <tohid_d.milan@yahoo.com>
2023-04-26 12:43:31 -0600

    modify finishup page

--- src/components/AddonsCard.js
+++ src/components/AddonsCard.js
@@ -7,8 +7,7 @@ function AddonsCard({ id, name, description, monthlyFee, yearlyFee }) {
         <div className="flex items-center">
             <input
                 className="relative mr-[15px] w-[25px] h-[25px] appearance-none border rounded-md checked:bg-[#473dff]
-                checked:before:content-['\2713'] checked:before:absolute checked:before:text-white checked:before:text-[16px]
-                checked:before:left-[0px] checked:before:right-[0px]"
+                checked:before:content-['\2713'] checked:before:leading-[20px] checked:before:text-[30px] checked:before:text-white"
                 id={id}
                 type="checkbox" />
             <div className="flex flex-col mr-[20px]">
--- src/components/FinishingUp.js
+++ src/components/FinishingUp.js
@@ -2,38 +2,30 @@
 function FinishingUp() {
     return <div className="relative -top-[80px] mx-[20px] mb-[50px] px-[30px] py-[30px] bg-white rounded-xl">
         <div>
-            <h1 className="text-[30px] font-bold">Finishing up</h1>
-            <p className="mt-[10px] pr-[80px] text-[#9699ab]">Double-check everything looks OK before confirming.</p>
-            <div>
-                <div className="flex">
+            <h1 className="text-[32px] font-bold">Finishing up</h1>
+            <p className="mt-[10px] text-[#9699ab] text-[20px] font-medium">Double-check everything looks OK before confirming.</p>
+            <div className="mt-[20px] mb-[30px] px-[15px] py-[20px] bg-[#fafbff] rounded-xl">
+                <div className="flex justify-between items-center mb-[20px] pb-[20px] border-b-2">
                     <div className="flex flex-col">
-                        <span>Arcade(Monthly)?</span>
-                        <button>Change</button>
+                        <span className="font-bold">Arcade (Monthly)</span>
+                        <a className="text-[#9699ab] underline leading-none font-bold" href="#">Change</a>
                     </div>
-                    <span>$9/mo?</span>
+                    <span className="font-bold">$9/mo</span>
                 </div>
-                <div className="flex">
-                    <span>Online service?</span>
-                    <span>+$1/mo?</span>
+                <div className="flex justify-between items-center mb-[10px]">
+                    <span className="text-[#9699ab] font-medium">Online service</span>
+                    <span className="font-medium">+$1/mo</span>
                 </div>
-                <div className="flex">
-                    <span>Larger storage?</span>
-                    <span>+$2/mo?</span>
-                </div>
-                <div className="flex">
-                    <span>Total(per month)?</span>
-                    <span>+$12/mo?</span>
+                <div className="flex justify-between items-center">
+                    <span className="text-[#9699ab] font-medium">Larger storage</span>
+                    <span className="font-medium">+$2/mo</span>
                 </div>
             </div>
-            <form className="flex flex-col items-center mt-[20px]">
-                <label className="text-[14px]" htmlFor="name">Monthly/Yearly</label>
-                <input
-                    className="px-[15px] py-[10px] block border w-full rounded"
-                    id="name"
-                    placeholder="e.g. Stephen King"
-                    type="checkbox"
-                    required />
-            </form>
+            <div className="flex justify-between items-center">
+                <span className="text-[#9699ab] font-medium">Total (per month)</span>
+                <span className="text-[#473dff] text-[20px] font-bold">+$12/mo</span>
+            </div>
+            
         </div>
     </div>
 }
